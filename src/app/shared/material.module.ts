28f7764e2a2d183ff0c2001e0f36f11ffaf58c4a
Orlando <osierra@level6.co>
2021-06-12 18:30:17 -0400

    Creating shared, material modules and toaster service

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,89 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {OverlayModule} from '@angular/cdk/overlay';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    OverlayModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSnackBarModule,
+    MatListModule,
+    MatDividerModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatAutocompleteModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    OverlayModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSnackBarModule,
+    MatListModule,
+    MatDividerModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatAutocompleteModule
+  ]
+})
+export class MaterialModule {
+}
